43e6f8b884d9737dcd4a4a31112caa8ee2fd1dad
Andrew Schlabach <andrew.schlabach@hotmail.com>
2025-10-23 11:13:12 -0400

    fixed some lsp issues

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,15 +3,15 @@ return {
   opts = {
     servers = {
 
-      verible = {
-        cmd = { "verible-verilog-ls",
-                "--rules=-line-length,-no-trailing-spaces",
-              },
-        filetypes = { "verilog", "systemverilog" },
-        root_dir = function(fname)
-          return require("lspconfig.util").root_pattern(".git")(fname) or vim.fn.getcwd()
-        end,
-      },
+      -- verible = {
+      --   cmd = { "verible-verilog-ls",
+      --           "--rules=-line-length,-no-trailing-spaces",
+      --         },
+      --   filetypes = { "verilog", "systemverilog" },
+      --   root_dir = function(fname)
+      --     return require("lspconfig.util").root_pattern(".git")(fname) or vim.fn.getcwd()
+      --   end,
+      -- },
       --
       pyright = {
         settings = {
@@ -26,5 +26,13 @@ return {
         },
       },
     },
+
+
+    setup = { -- Mason cannot handle verible, must do it manually.
+      verible = function(_, opts)
+        require("lspconfig").verible.setup(opts)
+        return true  -- Return true to prevent default setup
+      end,
+    }
   },
 }
